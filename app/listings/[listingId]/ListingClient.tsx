ccd051cc6ca7689a69a45b2ee244e4a6bfb5f6f5
rodri10901 <rodrigabales10901@gmail.com>
2023-11-27 20:08:34 +0800

    modified:   .env new file:   app/actions/getReservations.ts new file:   app/api/Reservations/route.ts new file:   app/components/Inputs/Calendar.tsx new file:   app/components/listings/ListingCategory.tsx modified:   app/components/listings/ListingHead.tsx new file:   app/components/listings/ListingInfo.tsx new file:   app/components/listings/ListingReservation.tsx modified:   app/globals.css modified:   app/listings/[listingId]/ListingClient.tsx modified:   package-lock.json modified:   package.json

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,11 +2,25 @@
 
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
+import ListingReservation from "@/app/components/listings/ListingReservation";
 import { categories } from "@/app/components/navbar/Categories";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
-import { useMemo } from "react";
+import axios from "axios";
+import { differenceInCalendarDays, eachDayOfInterval } from "date-fns";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Range } from "react-date-range";
+import toast from "react-hot-toast";
 
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+};
+ 
 interface ListingClientProps {
     reservation?: Reservation[];
     listing: SafeListing & {
@@ -17,8 +31,79 @@ interface ListingClientProps {
 
 const ListingClient: React.FC<ListingClientProps> = ({
     listing,
+    reservation = [],
     currentUser
 }) => {
+    const loginModal = useLoginModal();
+    const router = useRouter();
+
+    const disabledDates = useMemo(() => {
+        let dates: Date [] = [];
+
+        reservation.forEach((reservation) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate)
+            });
+
+            dates = [...dates, ...range];
+        });
+
+        return dates;
+    }, [reservation]);
+
+    const [isLoading, setIsloading] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(listing.price);
+    const[dateRange, setDateRange] = useState<Range>(initialDateRange);
+
+    const onCreateReservation = useCallback(() =>{
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        setIsloading(true);
+
+        axios.post('/api/reservation', {
+            totalPrice,
+            startDate: dateRange.startDate,
+            endDate: dateRange.endDate,
+            listingId: listing?.id
+        })
+        .then(() => {
+            toast.success('Listing reserved!');
+            setDateRange(initialDateRange);
+            // Redirect to /trips
+            router.refresh();
+        })
+        .catch(() => {
+            toast.error('Something went wrong.');
+        })
+        .finally(() => {
+            setIsloading(false);
+        })
+    }, [
+        totalPrice,
+        dateRange,
+        listing?.id,
+        currentUser,
+        loginModal,
+        router
+    ]);
+
+    useEffect(() => {
+        if (dateRange.startDate && dateRange.endDate) {
+            const dayCount = differenceInCalendarDays(
+                dateRange.endDate,
+                dateRange.startDate
+            );
+            if (dayCount && listing.price) {
+                setTotalPrice(dayCount * listing.price);
+            } else {
+                setTotalPrice(listing.price);
+            }
+        }
+    }, [dateRange, listing.price]);
+
     const category = useMemo(() => {
         return categories.find((item) =>
         item.label == listing.category);
@@ -29,12 +114,47 @@ const ListingClient: React.FC<ListingClientProps> = ({
             <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col gap-6">
                     <ListingHead 
-                        title={listing}
+                        title={listing.title}
                         imageSrc={listing.imageSrc}
                         locationValue={listing.locationValue}
                         id={listing.id}
                         currentUser={currentUser}
                     />
+                    <div className="
+                        grid
+                        grid-cols-1
+                        md:grid-cols-7
+                        md:gap-10
+                        mt-6
+                    ">
+                    <ListingInfo 
+                        user={listing.user}
+                        category={category}
+                        description={listing.description}
+                        roomCount={listing.roomCount}
+                        guestCount={listing.guestCount}
+                        bathroomCount={listing.bathroomCount}
+                        locationValue={listing.locationValue}
+                    />
+                    <div
+                        className="
+                           order-first 
+                           mb-10
+                           md:order-last
+                           md-col-span-3
+                        "
+                    >
+                        <ListingReservation 
+                            price={listing.price}
+                            totalPrice={totalPrice}
+                            onChangeDate={(value) => setDateRange(value)}
+                            dateRange={dateRange}
+                            onSubmit={onCreateReservation}
+                            disabled={isLoading}
+                            disabledDates={disabledDates}
+                        />
+                    </div>
+                    </div>
                 </div>
             </div>
         </Container>
